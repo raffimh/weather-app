965ac40694ce2cb4036ca8432cb45523e35b4795
Raffi Maulana Hadi <32472372+raffimh@users.noreply.github.com>
2020-12-21 19:43:35 +0700

    Update app.js

--- app.js
+++ app.js
@@ -1,7 +1,7 @@
 const express = require('express')
 const https = require("https")
 const app = express()
-const port = 3000
+const port = 300
 const bodyParser = require("body-parser")
 require("dotenv").config()
 

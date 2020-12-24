d7be8310abe60a082766d50e388d5ef9dda829b5
Raffi Maulana Hadi <32472372+raffimh@users.noreply.github.com>
2020-12-24 20:43:16 +0700

    Update app.js

--- app.js
+++ app.js
@@ -1,8 +1,8 @@
 const express = require('express')
 const https = require("https")
-const app = express()
-const bodyParser = require("body-parser")
-require("dotenv").config()
+const app = express();
+const bodyParser = require("body-parser");
+require("dotenv").config();
 
 app.use(bodyParser.urlencoded({
     extended: true
